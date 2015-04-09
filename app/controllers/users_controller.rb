e82fdd377405055c89fac7a4eee857f6846879f3
Kevin Orfas <anastasios.orfanidis@gmail.com>
2015-04-09 13:54:53 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -47,21 +48,13 @@ class UsersController < ApplicationController
   end
   
   private
+  
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-  
   # Confirms the correct user.
   def correct_user
       @user = User.find(params[:id])
